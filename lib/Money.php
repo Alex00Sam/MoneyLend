27b869a6db6641e869e211b4fea740bb007f5083
Alex00Sam <alsa00@mail.ru>
2017-04-26 20:04:14 +0300

    Update(new class)

---
+++ lib/Money.php
@@ -0,0 +1,11 @@
+<?php
+class Money extends \atk4\data\Model {
+	public $table = 'money';	
+	function init() {
+		parent::init();
+		$this->addField('type');
+		$this->addField('amount');
+		$this->addField('date');
+		$this->hasOne('friends_id');
+	}
+}
