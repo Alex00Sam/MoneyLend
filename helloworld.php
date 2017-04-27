9fafec5858420be127e385ff2dae2386550af7fa
Alex00Sam <alsa00@mail.ru>
2017-04-27 17:16:09 +0300

    fix

--- helloworld.php
+++ helloworld.php
@@ -1,12 +1,4 @@
 <?php
-require 'vendor/autoload.php';
 
-//New app//
-$app=new \atk4\ui\app('My first app');
-///////////
-
-//Layout//
-$app->initLayout('Centered');
-//////////
-$app->add('HelloWorld');
+echo ("Hello world");
 
