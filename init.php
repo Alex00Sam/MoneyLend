985dd9a276bbea51c70abcd4190ba79d5749010f
Alex00Sam <alsa00@mail.ru>
2017-05-05 14:17:08 +0300

    cleardb connect

--- init.php
+++ init.php
@@ -11,4 +11,4 @@ $layout = $app->initLayout('Admin');
 
 //DB Connection//
 $db = new 
-\atk4\data\Persistence_SQL('mysql:dbname=money;host=localhost','MySite','12345');
+\atk4\data\Persistence_SQL('mysql:dbname=money;host=eu-cdbr-west-01.cleardb.com','b43963c1dcd91f','8ed0930d');
