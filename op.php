28f3086628a7b31dec0dba3ebff8c37baa4e9d8f
Alex00Sam <alsa00@mail.ru>
2017-05-05 16:39:00 +0300

    fix

--- op.php
+++ op.php
@@ -6,7 +6,7 @@
 	$grid->setModel(new money($db));
 	}
 	else {
-		$error=$app->layout->add('No info was found');
+		$error=$app->layout->add(['text','No info was found']);
 		$grid= $app->layout->add('CRUD');
 		$grid->setModel(new money($db));	
 	}
