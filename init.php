6edce5daff3f15ded11c1fc7745ac45423fd10cb
Alex00Sam <alsa00@mail.ru>
2017-05-05 15:29:43 +0300

    fix

--- init.php
+++ init.php
@@ -11,4 +11,4 @@ $layout = $app->initLayout('Admin');
 
 //DB Connection//
 $db = new 
-\atk4\data\Persistence_SQL('mysql:dbname=money;host=eu-cdbr-west-01.cleardb.com','b43963c1dcd91f','8ed0930d');
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_280a158781dc374;host=eu-cdbr-west-01.cleardb.com','b43963c1dcd91f','8ed0930d');
