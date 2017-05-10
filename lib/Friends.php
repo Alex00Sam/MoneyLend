5501a9754a5997004f2f11dd721bcb65a8ad7979
Alex00Sam <alsa00@mail.ru>
2017-05-10 21:30:39 +0300

    fix

--- lib/Friends.php
+++ lib/Friends.php
@@ -3,7 +3,7 @@ class friends extends \atk4\data\Model {
 	public $table = 'friends';	
 	function init() {
 		parent::init();
-		$this->addField(['name','email']);
-		$this->hasMany('money', money);
+		$this->addFields(['name','email']);
+		$this->hasMany('money');
 	}
 }
