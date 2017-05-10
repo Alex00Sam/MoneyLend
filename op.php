cc1b1d6cf0d10f953e875427880684ed53f4f015
Alex00Sam <alsa00@mail.ru>
2017-05-10 20:13:18 +0300

    lending form fix

--- op.php
+++ op.php
@@ -1,12 +1,12 @@
 <?php
 	require 'init.php';
 	
-	if (isset($GET["id"])){
+/*	if (isset($GET["id"])){
 	$grid= $app->layout->add('CRUD');
 	$grid->setModel(new money($db));
 	}
 	else {
-		echo "No info was found";
+		echo "No info was found"; */
 		$grid= $app->layout->add('CRUD');
 		$grid->setModel(new money($db));	
-	}
+//	}
