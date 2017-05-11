c18a4819dbe6c0f01efd08b3bfdefe2e25e52792
Alex00Sam <alsa00@mail.ru>
2017-05-11 16:45:06 +0300

    Update lend.php

--- lend.php
+++ lend.php
@@ -12,7 +12,7 @@
 	$grid= $app->layout->add('CRUD');
 	$grid->setModel(new Friends($db));
 	$grid->addAction('Update details',new \atk4\ui\jsExpression('document.location="op.php
-	// ?id="+$(this).closest("tr").data("id")
+	//?id="+$(this).closest("tr").data("id")
 	;'));
 ?>
 
