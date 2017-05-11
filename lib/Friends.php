2fd2e6a7ad6904264115bd021afda30677894b11
Alex00Sam <alsa00@mail.ru>
2017-05-11 16:34:36 +0300

    Update Friends.php

--- lib/Friends.php
+++ lib/Friends.php
@@ -4,6 +4,6 @@ class friends extends \atk4\data\Model {
 	function init() {
 		parent::init();
 		$this->addFields(['name','email']);
-		$this->hasMany('id', new money);
+		$this->hasMany('money');
 	}
 }
