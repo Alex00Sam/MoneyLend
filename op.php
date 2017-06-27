0e7cd1887cfd57096bd4e8feeb33d9f5a353b318
Alex00Sam <alsa00@mail.ru>
2017-06-27 14:21:34 +0300

    .

--- op.php
+++ op.php
@@ -7,6 +7,8 @@
 	}
 	else {
 		echo "No info was found"; */
-		$grid= $app->layout->add('CRUD');
-		$grid->setModel(new money($db));	
+		$crud= $app->layout->add('CRUD');
+		//$crud->setModel(new money($db));	
+		$crud->addColumn('name', new \atk4\ui\TableColumn\Link(['loan', 'contact_id'=>'{$id}']));
+		$crud->setModel($app->user->ref('money'), ['email', 'phone_number']);
 //	}
