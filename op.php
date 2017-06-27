d102af27ebce5e0d3aa18cfb8c28917a62987c52
Alex00Sam <alsa00@mail.ru>
2017-06-27 14:29:17 +0300

    fix

--- op.php
+++ op.php
@@ -10,5 +10,5 @@
 		$crud= $app->layout->add('CRUD');
 		//$crud->setModel(new money($db));	
 		$crud->addColumn('name', new \atk4\ui\TableColumn\Link(['loan', 'contact_id'=>'{$id}']));
-		$crud->setModel($app->user->ref('Friends'), ['email', 'phone_number']);
+		$crud->setModel($app->user->ref('Friends'), ['email']);
 //	}
