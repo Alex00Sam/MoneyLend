c38c41f074418301dde11c4f9509b17361e9a405
Alex00Sam <alsa00@mail.ru>
2017-07-05 18:23:59 +0300

    Update friends.php

--- lib/friends.php
+++ lib/friends.php
@@ -3,7 +3,7 @@ class Friends extends \atk4\data\Model {
 	public $table = 'friends';
 	function init() {
 		parent::init();
-		$sum=$this->hasMany('Money', new Money())->addCondition('type', '+')->addField('total', ['aggregate'=>'sum', 'field'=>'amount']);
+		$sum=$this->hasMany('Money', new Money())->addCondition('type', '+'))->addField('total', ['aggregate'=>'sum', 'field'=>'amount']);
 		$this->addFields(['name','email','phone']);
 	//этого я не понял :(  	$friend->addParagraph('total', ['aggregate'=>'sum', 'field'=>'amount']);
 
