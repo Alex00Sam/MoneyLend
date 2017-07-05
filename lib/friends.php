6874fa85251fb3dd008e4a40f35348cb727edb6b
Alex00Sam <alsa00@mail.ru>
2017-07-05 19:18:46 +0300

    Update friends.php

--- lib/friends.php
+++ lib/friends.php
@@ -3,8 +3,8 @@ class Friends extends \atk4\data\Model {
 	public $table = 'friends';
 	function init() {
 		parent::init();
-		$borrowed=$this->hasMany('Borrowed', (new Money($this->persistence))->addCondition('type', 'new lend'))->addField('total_borrowed', ['aggregate'=>'sum', 'field'=>'amount']);
-		$returned=$this->hasMany('Returned', (new Money($this->persistence))->addCondition('type', 'new return'))->addField('total_returned', ['aggregate'=>'sum', 'field'=>'amount']);
+		$borrowed=$this->hasMany('Money', (new Money($this->persistence))->addCondition('type', 'new lend'))->addField('total_borrowed', ['aggregate'=>'sum', 'field'=>'amount']);
+		$returned=$this->hasMany('Money', (new Money($this->persistence))->addCondition('type', 'new return'))->addField('total_returned', ['aggregate'=>'sum', 'field'=>'amount']);
 		$this->addFields(['name','email','phone']);
 
 
