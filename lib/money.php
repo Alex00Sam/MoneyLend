a9448c644d457167fd82b75d4084d1112fc367ad
Alex00Sam <alsa00@mail.ru>
2017-07-14 12:41:33 +0300

    test

--- lib/money.php
+++ lib/money.php
@@ -3,7 +3,7 @@ class Money extends \atk4\data\Model {
 	public $table = 'money';
 	function init() {
 		parent::init();
-		$this->addField('type',['enum'=>['+'=>'new lend','-'=>'new return']]);
+		$this->addField('type',['enum'=>['new lend','new return']]);
 		$this->addField('amount', ['type'=>'money']);
 		$this->addField('date',['type'=>'date']);
 		$this->hasOne('friends_id', new Friends());
