ccbc2b7348137dafecfd89ccbc7c75df4a44d07f
Alex00Sam <alsa00@mail.ru>
2017-07-24 14:29:42 +0300

    fix

--- loan.php
+++ loan.php
@@ -7,8 +7,8 @@
 	$friend->load($app->stickyGet('friends_id'));
 	$loans = $friend->ref('Money');
 	$columns = $app->layout->add(['ui'=>'segment'])->add(new \atk4\ui\Columns('divided'));
-	$crud = $columns->addColumn();
+	$column = $columns->addColumn();
 	$ispaid = ['positive'=>['new return'],'negative'=>['new return']];
-	$crud->add('CRUD')->setModel(new Money($db))
+	$crud = $column->add('CRUD')->setModel(new Money($db))
 	->addColumn('status', new \atk4\ui\TableColumn\Status($ispaid));
 	//$reminder = $columns->addColumn()->add(new ReminderBox())->setModel($loans);
