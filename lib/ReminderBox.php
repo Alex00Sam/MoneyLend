5c5f01422b2ab4c7034223ef6bcb241644133560
Alex00Sam <alsa00@mail.ru>
2017-07-25 20:08:35 +0300

    fix

--- lib/ReminderBox.php
+++ lib/ReminderBox.php
@@ -6,7 +6,7 @@ class ReminderBox extends \atk4\ui\View {
      */
     public function setModel(\atk4\data\Model $friend) {
         $this->add('Header')->set('Please repay my loan, '.$friend['name']);
-        $this->add('Text')->addParagraph('I have loaned you a total of'. $friend['total_borrowed']. 'from which you still owe me'.$friend['total_borrowed']-$friend['total_returned']. 'Please pay back!');
+        $this->add('Text')->addParagraph('I have loaned you a total of' . $friend['total_borrowed'] . 'from which you still owe me' . ($friend['total_borrowed']-$friend['total_returned']) . 'Please pay back!');
         $this->add('Text')->addParagraph('Thanks!');
     }
 }
