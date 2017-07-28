2647e07601236d8b7aee7bbd3528bbed676c5abd
Alex00Sam <alsa00@mail.ru>
2017-07-28 13:08:00 +0300

    fix

--- lib/friends.php
+++ lib/friends.php
@@ -3,8 +3,8 @@ class Friends extends \atk4\data\Model {
 	public $table = 'friends';
 	function init() {
 		parent::init();
-		$this->hasMany('Borrowed',new Borrowed());
-		$this->hasMany('Returned',new Returned());
+		$this->hasMany('Borrowed',new Borrowed())->addField('total_borrowed', ['aggregate'=>'sum', 'field'=>'amount']);
+		$this->hasMany('Returned',new Returned())->addField('total_returned', ['aggregate'=>'sum', 'field'=>'amount']);
 		$this->addFields(['name','email','phone']);
 	}
 }
