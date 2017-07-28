7f36d228fe67740bb91fb9623b2114e415657cc7
Alex00Sam <alsa00@mail.ru>
2017-07-28 17:20:45 +0300

    test

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 require 'init.php';
-//Form//
+
 $intro = $app->layout->add('Header')->set('Welcome to Money Lending App, where you can manage your friend loans and their returnings. Enjoy!');
 $form = $app->layout->add('Form');
 $form->setModel(new Friends($db));
-
 $form->onSubmit(function($form) {
   $form->model->save();
-  return $form->success('Record updated');
-
+  return $form->success('You have successfully added a new friend!');
 });
-$layout->add(['ui'=>'hidden divider']);
-$crud = $app->layout->add('CRUD');
 
+$layout->add(['ui'=>'hidden divider']);
+
+$crud = $app->layout->add('CRUD');
 $crud->addColumn('name', new \atk4\ui\TableColumn\Link('loan.php?friends_id={$id}'));
 $crud->setModel(new Friends($db));
--- lib/ReminderBox.php
+++ lib/ReminderBox.php
@@ -1,9 +1,7 @@
 <?php
 class ReminderBox extends \atk4\ui\View {
     public $ui='piled segment';
-    /**
-     * Specify which contact to remind about
-     */
+
     public function setModel(\atk4\data\Model $friend) {
         $this->add('Header')->set('Please repay my loan, '.$friend['name']);
         $this->add('Text')->addParagraph('I have loaned you a total of ' . $friend['total_borrowed'] . '€ from which you still owe me ' . ($friend['total_borrowed']-$friend['total_returned']) . '€. Please pay back!');
