01f92f62dc8fde3a27cfe846ea88fc92e040d007
Alex00Sam <alsa00@mail.ru>
2017-07-28 13:09:21 +0300

    fix

--- loan.php
+++ loan.php
@@ -15,7 +15,7 @@
 	$crud1 = $column->add('CRUD');
   $crud1->setModel($borrowed,['amount','date']);
 	$crud2 = $column->add('CRUD');
-	$crud2->setModel($borrowed,['amount','date']);
+	$crud2->setModel($returned,['amount','date']);
 
 	$column2 =  $columns->addColumn();
  	$reminder = $column2->add('Header')->set('Here you have reminder message for your friend. If you will, you can send it to him.')->add(new ReminderBox())->setModel($friend);
